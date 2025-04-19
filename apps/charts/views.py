1e58a398875951d3820987f7a9fce970abc6b9f4
mominur-helios <mominur.rahman@heliosinsider.com>
2025-04-19 17:58:56 +0600

    Refactor code and add jazzmin

---
+++ apps/charts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.core import serializers
+from apps.pages.models import *
+
+# Create your views here.
+
+def index(request):
+  products = serializers.serialize('json', Product.objects.all())
+  context = {
+    'parent': 'apps',
+    'segment': 'charts',
+    'products': products
+  }
+  return render(request, 'charts/index.html', context)
